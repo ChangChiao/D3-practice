ce9f5d1cfe80394913d1996fdce90b370a92853c
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-09-26 09:03:09 +0800

    feat: add material icon

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,11 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p class="title">overview works!</p>`,
+  imports: [CommonModule, MatIconModule],
+  template: `
+    <div>
+      <mat-icon
+        aria-hidden="false"
+        aria-label="Example home icon"
+        fontIcon="home"
+      ></mat-icon>
+      <p class="title">overview works!</p>
+    </div>
+  `,
   styleUrls: ['./overview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
