5c6cf83859617091f1e96e68c40a8446a96be983
JoeCT_Chang <JoeCT.Chang@moxa.com>
2023-09-27 08:44:30 +0800

    feat: add custom icon

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
@@ -8,11 +8,19 @@ import { appRoutes } from './app.routes';
 import { TODO_KEY, TodoStore } from './store';
 import { provideState } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
+import { IconRegistryService } from './service/icon-registry.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideHttpClient(),
     // provideState(TODO_KEY, TodoStore),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (iconRegistryService: IconRegistryService) => () =>
+        iconRegistryService.init(),
+      deps: [IconRegistryService],
+      multi: true,
+    },
   ],
 };
--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -12,7 +12,10 @@ import { MatIconModule } from '@angular/material/icon';
         aria-hidden="false"
         aria-label="Example home icon"
         fontIcon="home"
+        class="green"
       ></mat-icon>
+      <mat-icon class="green" svgIcon="fog"></mat-icon>
+      <mat-icon svgIcon="moon"></mat-icon>
       <p class="title">overview works!</p>
     </div>
   `,
